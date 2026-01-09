de3a807f7149a40407edd24f77933db37c8ddcfc
Sieunhanom15 <87540541+Romet0123@users.noreply.github.com>
2026-01-10 00:36:28 +0700

    Update Audio Mixer & Volume 2

--- GameCulturesA3/Assets/Scripts/UI/PauseMenuUI.cs
+++ GameCulturesA3/Assets/Scripts/UI/PauseMenuUI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f5108d07f2bdbd0258d067b32948d37ad2d0a9f7e6a4d4d6f5c635045547368
-size 2861
+oid sha256:eb5d928d870f1c959edf087b048e319eeceaa1eb78a27567b2c1c45c15caf51f
+size 2488
