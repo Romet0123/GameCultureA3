9b49aaee38a17636275e9af49329e7de558b4cfd
Sieunhanom15 <87540541+Romet0123@users.noreply.github.com>
2026-01-16 01:14:38 +0700

    waiting to fix characters

---
+++ GameCulturesA3/Assets/Scripts/Gameplay/CharacterAnimationFlow.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e3d6cd638e707cb46e5e6545608a976418be289c1cac4fef7ad9cfc229f3adf2
+size 309
--- GameCulturesA3/Assets/Scripts/Gameplay/PrayerController.cs
+++ GameCulturesA3/Assets/Scripts/Gameplay/PrayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34b12f43d4c28efa63b434a7ad23cd59208f4e3e8b059436cd12063e1b6be6de
-size 12102
+oid sha256:f3e5357fd19d6642628819e248d880ad71365aab996c3dded1795ae01ff104d4
+size 11986
